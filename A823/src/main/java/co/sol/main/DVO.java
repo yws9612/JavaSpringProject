8eb72060e4906e112301adac88314ccc26791110
Seoyoung <seoyoung9710@gmail.com>
2022-08-24 15:20:25 +0900

    빈클래스 생성_서영_0824
    0824_빈클래스 생성_일단 현재 db 테이블이랑 일치하게 전부 생성함. 추후에 필요없는 클래스는 삭제 필요

---
+++ A823/src/main/java/co/sol/main/DVO.java
@@ -0,0 +1,16 @@
+package co.sol.main;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class DVO {
+	
+	private int u_no;
+	private int d_height;
+	private int d_weight;
+	private int d_bmi;
+	private Date d_date;
+	
+}
