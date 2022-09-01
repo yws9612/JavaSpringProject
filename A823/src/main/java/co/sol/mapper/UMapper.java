55d62d987256a8b955ddc63b67d4ff003386a0ce
nonameleel <xcvm92@gmail.com>
2022-09-01 12:33:35 +0900

    UMapper에 새비밀번호 관련 함수, sql 추가_송이
    0901

--- A823/src/main/java/co/sol/mapper/UMapper.java
+++ A823/src/main/java/co/sol/mapper/UMapper.java
@@ -7,6 +7,7 @@ public interface UMapper {
 //	public void Leave(UVO user);
 //	public String getId(UVO user);
 //	public String getPw(UVO user);
+//	public void newPw(UVO user);
 //	public UVO getUser(UVO user);
 //	public void addDiscord(UVO user, DVO discord);
 //	public void getDiscord(UVO user);
